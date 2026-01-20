1af8737b8dbd82b59d4c06e945e8917910a1d803
bhuvana <mailbox.of.bhuvana@gamil.com>
2026-01-20 19:01:55 +0530

    update UI

--- src/APIs/axiosInstance.js
+++ src/APIs/axiosInstance.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   timeout: 8000,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2QzMDE5MTRhZGRiN2MyNjk4ZDkxMDI1YTA0NjFiNyIsIm5iZiI6MTc2ODg5MzkzNS4wOTMsInN1YiI6IjY5NmYyZGVmMWQ4NWRiYTRmNjdhYmEyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hR0H90MA12eUqXWUV00gEXeE_82JJkx6SxmEL0z1jSY`,
+    Authorization: `Bearer  ${import.meta.env.VITE_TMDB_TOKEN}`,
   },
 });
 
